e3457f2dbd63f64693ed197cad548b792fa893f5
Jistoria <maestrojgcm@gmail.com>
2026-01-16 19:25:10 -0500

    main

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from src.features.ocr_updates.consumer import consume_ocr_finalized
 from src.features.validation.router import router as validation_router
 from src.features.search.router import router as search_router
 from src.features.storage.router import router as storage_router
+from src.features.templates.router import router as template_router
 from src.core.storage import storage_instance
 
 
@@ -66,6 +67,8 @@ app.add_middleware(
 app.include_router(validation_router)
 app.include_router(search_router)
 app.include_router(storage_router)
+
+app.include_router(template_router)
 
 @app.get("/")
 def health_check():
